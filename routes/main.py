5f38b4f29c1935431ce5aaa595e24b6f4bc2f52d
Alya Faisal <alyahanisa@gmail.com>
2026-02-02 17:42:19 +0800

    deleted routes folder, moved files in routes to blueprints

--- routes/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, session
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def home():
-    username = session.get('username')
-    return render_template('home.html', username=username)
