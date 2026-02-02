72297240d786cdf0eff2c7c3372c9b44050eb971
Alya Faisal <alyahanisa@gmail.com>
2026-02-02 17:16:33 +0800

    database handler moved to extensions

--- database.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-
-# # configure SQL Alchemy
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy()
-
