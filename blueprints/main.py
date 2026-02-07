6cc615a5c56352ead4d3733d6f2c18cbe55559bd
Alya Faisal <alyahanisa@gmail.com>
2026-02-07 20:07:47 +0800

    deleted kelly's files of tasks assigned to siti and alya

--- blueprints/main.py
+++ blueprints/main.py
@@ -10,16 +10,3 @@ def index():
     username = session.get('username')
     items = Item.query.all()
     return render_template("index.html", items=items)
-
-@main.route("/listings")
-@login_required
-def view_listings():
-    listings = Item.query.all()
-    return render_template("listing_list.html", listings=listings)
-
-@main.route("/messages")
-@login_required
-def view_messages():
-    # Get messages where the current user is the receiver
-    messages = ChatMessage.query.filter_by(receiver_id=current_user.id).all()
-    return render_template("messages.html", messages=messages)
